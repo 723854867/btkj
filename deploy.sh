c2064eb40b76bc163cae69af547ac61931a019e4
ahab-notebook <723854867@qq.com>
2017-07-26 16:59:34 +0800

    ahab

--- deploy.sh
+++ deploy.sh
@@ -18,15 +18,15 @@ git pull
 git push origin sj1.0
 echo "btkj sync finish, start build..."
 mvn clean install -Dmaven.test.skip -P${env}
-scpconfig${test}
-scpcourier${test}
-scpuser${test}
-scpmaster${test}
-scpvehicle${test}
-scpnonauto${test}
-scpcommunity${test}
-scpbaotuvehicle${test}
-scpbihuvehicle${test}
-scplebaobavehicle${test}
-scpstatistics${test}
-scppayment${test}
+scpconfig${env}
+scpcourier${env}
+scpuser${env}
+scpmaster${env}
+scpvehicle${env}
+scpnonauto${env}
+scpcommunity${env}
+scpbaotuvehicle${env}
+scpbihuvehicle${env}
+scplebaobavehicle${env}
+scpstatistics${env}
+scppayment${env}
