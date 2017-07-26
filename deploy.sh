ff371f37def10f77a7874e5c285929eb7e96f0f8
ahab-notebook <723854867@qq.com>
2017-07-26 17:37:36 +0800

    ahab

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "build test project..."
+env=$1
+IP="116.62.111.191"
+ENV="online"
+if [ "${env}"x = "test" ]; then
+	IP="101.37.30.26"
+	ENV="test"
+fi
+echo "build online project..."
 cd /f/work/workspace/eclipse/rapid
 echo "rapid git sync start..."
 git add ./*
@@ -16,18 +23,18 @@ git commit -m "ahab"
 git pull
 git push origin sj1.0
 echo "btkj sync finish, start build..."
-mvn clean install -Dmaven.test.skip -Ptest
+mvn clean install -Dmaven.test.skip -P${ENV}
 
 echo "start upload to server..."
-scp F:/work/workspace/eclipse/btkj/btkj-config/btkj-config-deploy/target/config-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-courier/btkj-courier-deploy/target/courier-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-user/btkj-user-deploy/target/user-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-master/btkj-master-deploy/target/master-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-vehicle/btkj-vehicle-deploy/target/vehicle-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-nonauto/btkj-nonauto-deploy/target/nonauto-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-community/btkj-community-deploy/target/community-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-baotu-vehicle/btkj-baotu-vehicle-deploy/target/baotu-vehicle-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-bihu-vehicle/btkj-bihu-vehicle-deploy/target/bihu-vehicle-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-lebaoba-vehicle/btkj-lebaoba-vehicle-deploy/target/lebaoba-vehicle-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-statistics/btkj-statistics-deploy/target/statistics-service.tar.gz root@101.37.30.26:/root/dubbo
-scp F:/work/workspace/eclipse/btkj/btkj-payment/btkj-payment-deploy/target/payment-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-config/btkj-config-deploy/target/config-service.tar.gz root@${IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-courier/btkj-courier-deploy/target/courier-service.tar.gz root@{IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-user/btkj-user-deploy/target/user-service.tar.gz root@{IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-master/btkj-master-deploy/target/master-service.tar.gz root@{IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-vehicle/btkj-vehicle-deploy/target/vehicle-service.tar.gz root@{IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-nonauto/btkj-nonauto-deploy/target/nonauto-service.tar.gz root@{IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-community/btkj-community-deploy/target/community-service.tar.gz root@{IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-baotu-vehicle/btkj-baotu-vehicle-deploy/target/baotu-vehicle-service.tar.gz root@{IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-bihu-vehicle/btkj-bihu-vehicle-deploy/target/bihu-vehicle-service.tar.gz root@{IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-lebaoba-vehicle/btkj-lebaoba-vehicle-deploy/target/lebaoba-vehicle-service.tar.gz root@1{IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-statistics/btkj-statistics-deploy/target/statistics-service.tar.gz root@{IP}:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-payment/btkj-payment-deploy/target/payment-service.tar.gz root@{IP}:/root/dubbo
---
+++ deploy_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "build test project..."
+cd /f/work/workspace/eclipse/rapid
+echo "rapid git sync start..."
+git add ./*
+git commit -m "ahab"
+git pull
+git push origin master
+echo "rapid sync finish, start build..."
+mvn clean install -Dmaven.test.skip
+
+cd /f/work/workspace/eclipse/btkj
+echo "btkj git sync start..."
+git add ./*
+git commit -m "ahab"
+git pull
+git push origin sj1.0
+echo "btkj sync finish, start build..."
+mvn clean install -Dmaven.test.skip -Ptest
+
+echo "start upload to server..."
+scp F:/work/workspace/eclipse/btkj/btkj-config/btkj-config-deploy/target/config-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-courier/btkj-courier-deploy/target/courier-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-user/btkj-user-deploy/target/user-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-master/btkj-master-deploy/target/master-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-vehicle/btkj-vehicle-deploy/target/vehicle-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-nonauto/btkj-nonauto-deploy/target/nonauto-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-community/btkj-community-deploy/target/community-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-baotu-vehicle/btkj-baotu-vehicle-deploy/target/baotu-vehicle-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-bihu-vehicle/btkj-bihu-vehicle-deploy/target/bihu-vehicle-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-lebaoba-vehicle/btkj-lebaoba-vehicle-deploy/target/lebaoba-vehicle-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-statistics/btkj-statistics-deploy/target/statistics-service.tar.gz root@101.37.30.26:/root/dubbo
+scp F:/work/workspace/eclipse/btkj/btkj-payment/btkj-payment-deploy/target/payment-service.tar.gz root@101.37.30.26:/root/dubbo
